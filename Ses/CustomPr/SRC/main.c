844109a2edc1ec719ea9e1665a6a1ebf72a983f8
KSG <indra1469@naver.com>
2021-10-30 04:53:32 +0900

    KSG : Ses 셋팅 최적화 및 설정파일저장, 소스파일 최적화

--- Ses/CustomPr/SRC/main.c
+++ Ses/CustomPr/SRC/main.c
@@ -7,9 +7,10 @@
 #include "nrf_delay.h"
 
 
-void in_pin_handler(nrf_drv_gpiote_pin_t pin,	nrf_gpiote_polarity_t	action)
+void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-	//nrf_drv_gpiote_out_toggle(13);
+	// nrf_drv_gpiote_out_toggle(13);
+
 	switch (pin)
 	{
 		case 12:
@@ -22,28 +23,27 @@ void in_pin_handler(nrf_drv_gpiote_pin_t pin,	nrf_gpiote_polarity_t	action)
 	}
 }
 
-int	main()
+int main()
 {
-
 	gpioe_init();
 	gpioe_output_set(13, false);
-	gpioe_input_set(12,	true, 1, NRF_GPIO_PIN_PULLUP, in_pin_handler);
+	gpioe_input_set(12, true, 1, NRF_GPIO_PIN_PULLUP, in_pin_handler);
 
 
-	//gpio_input_init(12,	0);
+	//gpio_input_init(12, 0);
 	//gpio_output_init(13, 1);
-	//gpio_output_set(13,	1);
+	//gpio_output_set(13, 1);
 
 
-	while (1)
-	{
-		//if (gpio_input_read(12))
-		//{
-		//	gpio_output_set(13,	0);
-		//	nrf_delay_ms(1000);
-		//	gpio_output_set(13,	1);
-		//}
+	//while (1)
+	//{
+	//	if (gpio_input_read(12))
+	//	{
+	//		gpio_output_set(13, 0);
+	//		nrf_delay_ms(1000);
+	//		gpio_output_set(13, 1);
+	//	}
 
-		//nrf_delay_ms(50);
-	}
+	//	nrf_delay_ms(50);
+	//}
 }
